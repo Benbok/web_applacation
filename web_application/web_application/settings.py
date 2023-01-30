fb8c3507e4c0201621c9e23b4f0c1a4ce262915d
Banbok <artoem_k@mail.ru>
2023-01-30 14:19:29 +0500

    редактирование base.html

--- web_application/web_application/settings.py
+++ web_application/web_application/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/ultrasound_examination')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = []
 
 # Default primary key field type
